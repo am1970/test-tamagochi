68f3d1b5f4fe4fce5e0ed5a75e61b0a748b3e8ed
am1970 <tito53sa@gmail.com>
2019-03-06 14:52:02 +0200

    create migrations & models

---
+++ database/migrations/2019_03_06_122006_create_animals_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnimalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->mediumIncrements('id');
+            $table->string('name', 60)->unique();
+            $table->string('title', 60);
+            $table->string('image_path');
+            $table->timestamps();
+
+            $table->index(['name']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('animals');
+    }
+}
---
+++ database/migrations/2019_03_06_122017_create_attributes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttributesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attributes', function (Blueprint $table) {
+            $table->mediumIncrements('id');
+            $table->string('name', 60)->unique();
+            $table->string('title', 60);
+            $table->string('description')->nullable();
+            $table->timestamps();
+
+            $table->index(['name']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attributes');
+    }
+}
