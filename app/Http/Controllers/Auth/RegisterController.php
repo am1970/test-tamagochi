7b078b2f50b3058273ba49272f4248e4c2517666
am1970 <tito53sa@gmail.com>
2019-03-10 00:00:07 +0200

    updated main page

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/animal/choice';
 
     /**
      * Create a new controller instance.
---
+++ resources/views/animal-choice/animal-choice.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-header">Choice Animal</div>
+
+                    <div class="card-body" id="app">
+                        <tamagochi-app></tamagochi-app>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
