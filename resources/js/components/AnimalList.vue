bbc549630c618eea1deb1077fa953f660aa4e59a
am1970 <tito53sa@gmail.com>
2019-03-10 18:00:11 +0200

    animal choice

--- resources/js/components/AnimalList.vue
+++ resources/js/components/AnimalList.vue
@@ -1,16 +1,43 @@
 <template>
-    <div class="animal-list"></div>
+    <div class="animal-list">
+        <select v-model="selected">
+            <option v-for="animal in animals" :value="animal.id">{{animal.title}}</option>
+        </select>
+        <br><br>
+        <input type="button" value="Choice" @click="save(selected)">
+    </div>
 </template>
 
 <script>
     export default {
         data() {
             return {
-                animalList: []
+                selected: null,
+                animals: []
             }
         },
         mounted() {
-            console.log('Animal List mounted.')
+            axios.get('/animals')
+                .then((res) => {
+                    console.log(res.data);
+                    this.animals = res.data;
+                })
+                .catch((e) => {
+                    console.log(e);
+                });
+        },
+        methods: {
+            save: (selected) => {
+                axios.post('/animal', {animal_id: selected})
+                    .then((res) => {
+                        if(res.data.success == true) {
+                            window.location.href = '/animal';
+                        }
+                    })
+                    .catch((e) => {
+                        console.log(e);
+                    });
+            }
         }
     }
 </script>
